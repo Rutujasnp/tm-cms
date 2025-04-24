6adddd4272aded69ef16c50b6f5ce3add8ebfc05
Rutujasnp <rutujasnp@gmail.com>
2025-04-24 23:36:20 +0530

    initial commit

---
+++ src/main/java/com/hc/cms/TmCmsApplication.java
@@ -0,0 +1,13 @@
+package com.hc.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmCmsApplication.class, args);
+	}
+
+}
